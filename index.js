657c09efad680668c80462cb0b3e19494d6b04f4
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-06-25 18:18:17 -0600

    lookin' pretty gud

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 /*
 todo
-extract function that takes MongoClient.connect and url and returns a task
-use the same function for db.collection('sandwiches').insertMany
-chain these two operations
-
 chain the whole sequence for the "Now I'm going to insert a collection (initialized with an array), print out its contents, remove an item from that collection, and then print out the new contents" example 
-
-if this works all you need to do is make a post about Task, it's just a promise that you can map!
+    May have to hand the db down the chain in a data structure that holds both result and db
+        That way I can db.close in fork
+    It bothers me that I'm calling fold from inside a new task, shouldn't I be doing a traverse?
 */
 const  
     {MongoClient} = require('mongodb'),
     Task = require('data.task'),
+    {Success, Failure} = require('data.validation'),
+    fromNullableError = (err, succ) => err === null ? Success(succ) : Failure(err),
+    rejResFromNullableError = (rej, res) => (err, db) => fromNullableError(err, db).fold(rej, res),
+    mongoTask = (db, dbMethodCall) => new Task((rej, res) => {
+        dbMethodCall(db, rejResFromNullableError(rej, res))
+    }),
     mongoConnect = url => new Task((rej, res) => {
         MongoClient.connect(url, (err, db) => {  
-            if(err === null) {
-                res(db)
-            } else {
-                rej(err)
-            }
+            fromNullableError(err, db).fold(rej, res)
             db.close()
         })
     }),
-    listCollections = db => new Task((rej, res) => {
-        db.listCollections().toArray((err, db) => {  
-            if(err === null) {
-                res(db)
-            } else {
-                rej(err)
-            }
-        })
-    }),
+    listCollections = db => mongoTask(db, (db, cb) => db.listCollections().toArray(cb)),
     url = 'mongodb://localhost:27017/potato'
 
-mongoConnect(url).chain(listCollections).fork(console.error, items => {
-    console.log('****listing collections****')
-    console.log(items)
-    console.log('***************************')
-})
+mongoConnect(url)
+    .chain(listCollections)
+    .fork(console.error, items => {
+        console.log('****listing collections****')
+        console.log(items)
+        console.log('***************************')
+    })
